c098ced5abb70c2043a50e999b4cf51f54615306
oderflaY <a.valadez.isw@unipolidgo.edu.mx>
2025-09-10 10:38:48 -0600

    Integracion de tailwind
    Se integro la libreria de taind al proyecto por medio de nativewind

--- alivia.tech/App.tsx
+++ alivia.tech/App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "./global.css";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 justify-center items-center bg-blue-200">
+      <Text className="text-2xl font-bold text-white">
+        Hola, mundo con Tailwind! 🚀
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
